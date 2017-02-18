cba3d520c616ab0bd2a3ba8c9f6606b6cf984c90
TomarAditya <angular.aditya@gmail.com>
2017-02-19 03:12:41 +0530

    new

--- app.js
+++ app.js
@@ -19,12 +19,30 @@ todoController(app);
 
 
 //listen to ports
-var port = processs.env.PORT || 8080
+var express = require('express');
+
+var todoController = require('./controllers/todoController');
+
+var app = express();
+
+//Set up template 
+app.set('view engine' , 'ejs');
+
+//Static files
+//app.use(express.static('/public/assets'));
+
+ app.use('/assets' , express.static('assets'));
+//fire controller
+
+todoController(app);
+
+
+//listen to ports
+var port = process.env.PORT || 8080
 
 app.listen(port ,function(){
 	console.log("App Running");
 });
-
 
 
 
